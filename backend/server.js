1a0c2c0ee4ce78175f0e262b07d3f9e954622cf8
arpanp11 <arpanpatel2810@gmail.com>
2020-12-29 00:07:36 -0500

    user auth

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import colors from 'colors';
 
 import connectDB from './config/db.js';
 import productRoute from './routes/productRoute.js';
+import userRoute from './routes/userRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
@@ -12,11 +13,14 @@ connectDB();
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
 app.use('/api/products', productRoute);
+app.use('/api/users', userRoute);
 
 app.use(notFound);
 
