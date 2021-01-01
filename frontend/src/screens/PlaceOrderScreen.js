acca1d81d897f02c556117d94233e8ad826fcb03
arpanp11 <arpanpatel2810@gmail.com>
2021-01-01 18:44:44 -0500

    add create order functionality

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -5,12 +5,12 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import Message from '../components/Message';
 import CheckoutSteps from '../components/CheckoutSteps';
-// import { createOrder } from '../actions/orderActions';
+import { createOrder } from '../actions/orderActions';
 // import { ORDER_CREATE_RESET } from '../constants/orderConstants';
 // import { USER_DETAILS_RESET } from '../constants/userConstants';
 
 const PlaceOrderScreen = ({ history }) => {
-  //   const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
   const cart = useSelector((state) => state.cart);
 
@@ -36,31 +36,30 @@ const PlaceOrderScreen = ({ history }) => {
     Number(cart.taxPrice)
   ).toFixed(2);
 
-  //   const orderCreate = useSelector((state) => state.orderCreate);
-  //   const { order, success, error } = orderCreate;
+  const orderCreate = useSelector((state) => state.orderCreate);
+  const { order, success, error } = orderCreate;
 
-//   useEffect(() => {
-//     if (success) {
-//       history.push(`/order/${order._id}`);
-//       //   dispatch({ type: USER_DETAILS_RESET });
-//       //   dispatch({ type: ORDER_CREATE_RESET });
-//     }
-//     // eslint-disable-next-line
-//   }, [history, success]);
+  useEffect(() => {
+    if (success) {
+      history.push(`/order/${order._id}`);
+      //   dispatch({ type: USER_DETAILS_RESET });
+      //   dispatch({ type: ORDER_CREATE_RESET });
+    }
+    // eslint-disable-next-line
+  }, [history, success]);
 
   const placeOrderHandler = () => {
-    console.log('order summury');
-    // dispatch(
-    //   createOrder({
-    //     orderItems: cart.cartItems,
-    //     shippingAddress: cart.shippingAddress,
-    //     paymentMethod: cart.paymentMethod,
-    //     itemsPrice: cart.itemsPrice,
-    //     shippingPrice: cart.shippingPrice,
-    //     taxPrice: cart.taxPrice,
-    //     totalPrice: cart.totalPrice,
-    //   })
-    // );
+    dispatch(
+      createOrder({
+        orderItems: cart.cartItems,
+        shippingAddress: cart.shippingAddress,
+        paymentMethod: cart.paymentMethod,
+        itemsPrice: cart.itemsPrice,
+        shippingPrice: cart.shippingPrice,
+        taxPrice: cart.taxPrice,
+        totalPrice: cart.totalPrice,
+      })
+    );
   };
 
   return (
