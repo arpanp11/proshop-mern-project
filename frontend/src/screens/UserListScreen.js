90b0ceb3d898501f95caaca86322413ba7af9134
arpanp11 <arpanpatel2810@gmail.com>
2021-01-03 15:03:39 -0500

    product review function(frontend)

--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react';
 import { LinkContainer } from 'react-router-bootstrap';
 import { Table, Button } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
-
 import Message from '../components/Message';
 import Loader from '../components/Loader';
 import { listUsers, deleteUser } from '../actions/userActions';
@@ -25,7 +24,7 @@ const UserListScreen = ({ history }) => {
     } else {
       history.push('/login');
     }
-  }, [dispatch, history, userInfo, successDelete]);
+  }, [dispatch, history, successDelete, userInfo]);
 
   const deleteHandler = (id) => {
     if (window.confirm('Are you sure')) {
@@ -88,4 +87,5 @@ const UserListScreen = ({ history }) => {
     </>
   );
 };
+
 export default UserListScreen;
